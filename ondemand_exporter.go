eb36ee2a8ccf7f60f80263fc4e79f2fa6741c2d9
Andrew E. Bruno <aebruno2@buffalo.edu>
2020-06-04 12:14:57 -0400

    Fix typo

--- ondemand_exporter.go
+++ ondemand_exporter.go
@@ -67,7 +67,7 @@ func main() {
 	kingpin.Parse()
 
 	logger := promlog.New(promlogConfig)
-	level.Info(logger).Log("msg", "Starting gpfs_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Starting ondemand_exporter", "version", version.Info())
 	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
 	level.Info(logger).Log("msg", "Starting Server", "address", *listenAddr)
 
